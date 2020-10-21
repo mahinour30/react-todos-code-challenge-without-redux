3b4dcfb661af635ecdb1646845e5fbd683a73237
mahinour30 <mahinour1233@gmail.com>
2020-10-22 01:40:25 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,10 @@ import TodoForm from '../components/TodoForm';
 import TodoList from '../components/TodoList';
 
 function IndexPage() {
+
+  // the next line is to set a state for the input text comming from the user to create a todo item 
   const [inputText,setInputText]= useState('');
+  // the next line is to set a state for the todo list
   const [todos, setTodos] = useState([]);
   return (
     <main>
